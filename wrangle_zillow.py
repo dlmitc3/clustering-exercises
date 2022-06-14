1e098e15fd7d9ac2f50a5054a742c66f2da6f05c
David Mitchell <Davidlee.Mitchell2@Gmail.com>
2022-06-14 09:47:05 -0500

    wrangle_zillow.py

--- wrangle_zillow.py
+++ wrangle_zillow.py
@@ -3,60 +3,44 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, QuantileTransformer
+from env import host, username, password
+from sklearn.impute import SimpleImputer
+
+
+# Acquire
+
+# Get Connection
+def get_connection(db, username=username, host=host, password=password):
+    '''
+    This function uses my info from my env file to
+    create a connection url to access the Codeup SQL db.
+    '''
+    return f'mysql+pymysql://{username}:{password}@{host}/{db}'
+
+
+# Get Zillow Data===========================================================
 
 def acquire():
     '''
-    This function uses a SQL query to access the Codeup MySQL database and join 
-    together all the relevant data from the zillow database.
-    The data obtained includes all properties in the dataset which had a transaction in 2017.
-    The function caches a csv in the local directory for later use. 
     '''
-    # establish a filename for the local csv
-    filename = 'zillow.csv'
-    # check to see if a local copy already exists. 
-    if os.path.exists(filename):
-        print('Reading from local CSV...')
-        # if so, return the local csv
-        return pd.read_csv(filename)
-    # otherwise, pull the data from the database:
-    # establish database url
-    url = env.get_db_url('zillow')
-    # establish query
-    sql = '''
-            SELECT prop.*,
-                   pred.logerror,
-                   const.typeconstructiondesc,
-                   arch.architecturalstyledesc,
-                   land.propertylandusedesc,
-                   heat.heatingorsystemdesc,
-                   air.airconditioningdesc, 
-                   bldg.buildingclassdesc,
-                   story.storydesc
-              FROM properties_2017 prop
-                JOIN predictions_2017            pred  USING(parcelid)
-                LEFT JOIN typeconstructiontype   const USING(typeconstructiontypeid)
-                LEFT JOIN architecturalstyletype arch  USING(architecturalstyletypeid)
-                LEFT JOIN propertylandusetype    land  USING(propertylandusetypeid)
-                LEFT JOIN heatingorsystemtype    heat  USING(heatingorsystemtypeid)
-                LEFT JOIN airconditioningtype    air   USING(airconditioningtypeid)
-                LEFT JOIN buildingclasstype      bldg  USING(buildingclasstypeid)
-                LEFT JOIN storytype              story USING(storytypeid)
-              WHERE pred.transactiondate LIKE "2017%%"
-                AND pred.transactiondate in (
-                                             SELECT MAX(transactiondate)
-                                               FROM predictions_2017
-                                               GROUP BY parcelid
-                                             )
-                AND prop.latitude IS NOT NULL
-                AND prop.longitude IS NOT NULL;
-            '''
-    print('No local file exists\nReading from SQL database...')
-    # query the database and return the resulting table as a pandas dataframe
-    df = pd.read_sql(sql, url)
-    # save the dataframe to the local directory as a csv
-    print('Saving to local CSV... ')
-    df.to_csv(filename, index=False)
-    # return the resulting dataframe
+    
+    # Create SQL query
+    sql_query = '''SELECT *                    
+    FROM predictions_2017
+    JOIN properties_2017 USING (parcelid)
+    LEFT JOIN airconditioningtype USING (airconditioningtypeid)
+    LEFT JOIN architecturalstyletype USING (architecturalstyletypeid)
+    LEFT JOIN buildingclasstype USING (buildingclasstypeid)
+    LEFT JOIN heatingorsystemtype USING (heatingorsystemtypeid)
+    LEFT JOIN propertylandusetype USING (propertylandusetypeid)
+    LEFT JOIN storytype USING (storytypeid)
+    LEFT JOIN typeconstructiontype USING (typeconstructiontypeid)
+    WHERE latitude IS NOT Null
+        AND longitude IS NOT Null'''
+
+    # Read in DataFrame from Codeup db.
+    df = pd.read_sql(sql_query, get_connection('zillow'))
+
     return df
 
 def prepare(df):
